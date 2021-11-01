4909bbcc157c22a53062366e0a68e43db4910ec8
gszep <gregory.szep@gmail.com>
2021-11-01 17:29:57 +0000

    making progress on intro :)

--- figures/hysteresis.jl
+++ figures/hysteresis.jl
@@ -1,4 +1,5 @@
-using Pkg,Distributed,DifferentialEquations,Parameters,Polyhedra,StatsBase
+using Pkg,Distributed,DifferentialEquations,Parameters,Polyhedra
+using LinearAlgebra,StatsBase
 addprocs(4;exeflags="--project=$(Pkg.project().path)")
 
 @everywhere using DifferentialEquations
@@ -29,14 +30,21 @@ set_theme!(Theme(
 		yticksvisible  = false,
         xticklabelsvisible  = false,
         yticklabelsvisible  = false,
-    )
+		titlegap=25,
+    ),
+	Label = (
+		tellheight=false,
+		tellwidth=false
+	)
 ))
 
 figure = Figure(resolution = 72 .* (36,12) )
 for (i,condition) ∈ enumerate([[2,2],[0,2],[2,0]])
 	ax = Axis(figure[i,1],
+		title = i == 1 ? L"Protocol $p(t),{p \prime}(t)$" : "",
 		ylabel = i == 2 ? L"states $u(t)$" : "",
-		xlabel = i == 3 ? L"$t$" : ""
+		xlabel = i == 3 ? L"time $t$" : "",
+		ylabelpadding = 25,
 	)
 	for (prime,color) ∈ [([2,-1/2],RGBA(1.0,0.753,0.0,0.1)), ([-1/2,2],RGBA(0.0,0.69,0.941,0.1))]
 		ensemble = solve( EnsembleProblem(
@@ -49,18 +57,21 @@ for (i,condition) ∈ enumerate([[2,2],[0,2],[2,0]])
 			lines!(ax,trajectory.t,map(u->u[1],trajectory.u),linewidth=5,color=color)
 		end
 	end
+	ylims!(ax,(-5,5)); xlims!(ax,(0,5))
+	vlines!(ax, [2.5], linewidth=5,color=:gray,linestyle=:dash)
 end
 
 ax = Axis(figure[1:3,2],
-    ylabel = L"p\prime",
-    xlabel = L"p"
+	title = L"Steady State Distributions $P(u)$",
+    ylabel = L"conditon $p\prime$",
+    xlabel = L"conditon $p$"
 )
 
-Δp = 25
-Δ = Δp/9
+Δp = 15
+Δ = Δp/5
 
-U,V = Matrix{Float64}(undef,10,10),Matrix{Float64}(undef,10,10)
-for condition ∈ Iterators.product(range(-1/2,2,length=10),range(-1/2,2,length=10))
+N = 6
+for condition ∈ Iterators.product(range(-1/2,2,length=N),range(-1/2,2,length=N))
 	for (prime,color) ∈ [([2,-1/2],RGBA(1.0,0.753,0.0,0.1)), ([-1/2,2],RGBA(0.0,0.69,0.941,0.1))]
 		ensemble = solve( EnsembleProblem(
 				SDEProblem(f,σ,randn(2),(0.0,10.0),(t=prime,t′=condition,τ=5)),
@@ -68,20 +79,65 @@ for condition ∈ Iterators.product(range(-1/2,2,length=10),range(-1/2,2,length=
 			SRIW1(), EnsembleDistributed(),trajectories=200
 		)
 
-		density = StatsBase.normalize(fit(Histogram, (map(x->x.u[end][1]+Δp*condition[1],ensemble),map(x->x.u[end][2]+Δp*condition[2],ensemble)),nbins=10), mode=:density)
+		density = StatsBase.normalize(fit(Histogram, (map(x->-x.u[end][2]+Δp*condition[1],ensemble),map(x->x.u[end][1]+Δp*condition[2],ensemble)),nbins=10), mode=:density)
 		contourf!(ax,density,colormap=[RGBA(color.r,color.g,color.b,0.5)],levels=[0.1,0.5,1.0],mode=:relative)
-
-		x,y = Δp.*condition
-		poly!(ax,[Point(x-Δ,y-Δ),Point(x-Δ,y+Δ),Point(x+Δ,y+Δ),Point(x+Δ,y-Δ)],
-			color=RGBA(0,0,0,0),strokecolor=:black,strokewidth=1)
 	end
+
+	x,y = Δp.*condition
+	poly!(ax,[Point(x-Δ,y-Δ),Point(x-Δ,y+Δ),Point(x+Δ,y+Δ),Point(x+Δ,y-Δ)], color=RGBA(0,0,0,0),
+		strokecolor = all(condition .≈ [2,-1/2]) ? RGB(1.0,0.753,0.0) : all(condition .≈ [-1/2,2]) ? RGB(0.0,0.69,0.941) : :black,
+		strokewidth = all(condition .≈ [-1/2,2]) ? 5 : all(condition .≈ [2,-1/2]) ? 5 : 1)
+end
+
+N = 12
+U = Matrix{Float64}(undef,N,N)
+for (j,condition) ∈ enumerate(Iterators.product(range(-1/2,2,length=N),range(-1/2,2,length=N)))
+
+	u = Vector{Vector{Float64}}(undef,2)
+	for (i,(prime,color)) ∈ enumerate([([2,-1/2],RGBA(1.0,0.753,0.0,0.1)), ([-1/2,2],RGBA(0.0,0.69,0.941,0.1))])
+		ensemble = solve( EnsembleProblem(
+				SDEProblem(f,σ,randn(2),(0.0,10.0),(t=prime,t′=condition,τ=5)),
+			prob_func = (x,_,_) -> remake(x,u0=randn(length(x.u0)))),
+			SRIW1(), EnsembleDistributed(),trajectories=200
+		)
+		u[i] = map(i->StatsBase.median(map(x->-x.u[end][i],ensemble)),1:2)
+	end
+	U[j] = norm(u[1]-u[2])
 end
 
 ax = Axis(figure[1:3,3],
-    ylabel = L"p\prime",
-    xlabel = L"p"
+	title = L"Population Separation ${\Delta}u(p,{p \prime})$",
+    ylabel = L"conditon $p\prime$",
+    xlabel = L"conditon $p$"
 )
 
+contourf!(ax,range(-1/2,2,length=N),range(-1/2,2,length=N),U,colormap=:grayC)
+contour!(ax,range(-1/2,2,length=N),range(-1/2,2,length=N),U, levels=[0.55], mode=:relative, linewidth=5, color=:black )
+scatter!( ax, [0.13], [0.13], markersize=25, color=:white, strokecolor=:black, strokewidth=5 )
+
+Label(figure[1,1], L"p \ll {p \prime}",
+	color=RGB(0.0,0.69,0.941),
+	height=-100, width=700
+)
+
+Label(figure[1,1], L"p \gg {p \prime}",
+	color=RGB(1.0,0.753,0.0),
+	height=200, width=700
+)
+
+Label(figure[1,1], L"p \approx {p \prime}",
+	height=200, width=-50
+)
+
+Label(figure[2,1], L"p \ll {p \prime}",
+	color=RGB(0.0,0.69,0.941),
+	height=200, width=-50
+)
+
+Label(figure[3,1], L"p \gg {p \prime}",
+	color=RGB(1.0,0.753,0.0),
+	height=200, width=-50
+)
 figure
 
 save("figures/hysteresis.pdf",figure)
