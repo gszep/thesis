a8016eb1107fa4af5cac1482d12e6d11dac3eaa4
gszep <gregory.szep@gmail.com>
2022-02-08 10:39:04 +0000

    update eigenvalue spectrum figure

--- figures/laplacian-spectrum.jl
+++ figures/laplacian-spectrum.jl
@@ -1,49 +1,56 @@
-using DifferentialEquations,DSP
-using ForwardDiff,LinearAlgebra
-using CairoMakie,Colors,InvertedIndices
+using LinearAlgebra,StatsBase
+using CairoMakie,Colors
+
+⊗(A,B) = kron(A,B)
+⊕(A,B) = A ⊗ I(size(B,1)) + I(size(A,1)) ⊗ B
+
+function laplacian(N::Int)
+    ∇² = zeros(Int8,N,N)
+
+    # central differences
+    ∇²[diagind(∇²)] .= -2 
+    ∇²[diagind(∇²,1)] .= 1
+
+    # periodic boundary conditions
+    ∇²[1,end] = 1
+
+    return Symmetric(∇²)
+end
+
+function eigenspectrum(A::AbstractMatrix; edges::AbstractVector=-5:0.05:2, mode=:pdf)
+    return StatsBase.normalize( fit(Histogram,real(eigvals(A)),edges), mode=mode)
+end
 
 CairoMakie.activate!()
-
 set_theme!(Theme(
     Axis = (
         xgridvisible  = false,
 		ygridvisible  = false,
         xticksvisible  = false,
 		yticksvisible  = false,
-        xticklabelsvisible  = false,
+        # xticklabelsvisible  = false,
         yticklabelsvisible  = false,
     )
 ))
 
+J = [1 1; -3 -2]
+∇² = laplacian(2000)
+
+stable = eigenspectrum( ∇² )
+unstable = eigenspectrum( ∇² ⊗ Diagonal([1,100]) + I(size(∇²,1)) ⊗ J )
+equaldiff = eigenspectrum( ∇² ⊗ Diagonal([1,1]) + I(size(∇²,1)) ⊗ J )
+
 figure = Figure(resolution = 72 .* (7,7) )
 ax = Axis(figure[1,1],
     ylabel = L"Eigenvalue Distribution $P(\lambda)$",
-    xlabel = L"Scaled Eigenvalue $\frac{\lambda}{L}$"
+    xlabel = L"Eigenvalue $\Re$e$[\lambda]$"
 )
 
-λ = range(-5,1,length=200)
-function P(λ::Number)
-    return real(1 / ( 2π * √Complex(1-(λ/2+1)^2) ))
-end
+scatter!(stable, color=:gray, markersize=3)
+scatter!(equaldiff, color=:darkblue, markersize=3)
+scatter!(unstable, color=:lightblue, markersize=3)
 
-p = P.(λ)
-df = range(-5,1,length=length(p))[2] - range(-5,1,length=length(p))[1]
-band!(range(-5,1,length=length(p)),p/sum(p*df),0,color=RGBA(0.5,0,0,0.2))
-
-p = conv(P.(λ),P.(λ))
-df = range(-5,1,length=length(p))[2] - range(-5,1,length=length(p))[1]
-band!(range(-5,1,length=length(p)),p/sum(p*df),0,color=RGBA(0.5,0,0,0.2))
-
-p = conv(P.(λ),p)
-df = range(-5,1,length=length(p))[2] - range(-5,1,length=length(p))[1]
-band!(range(-5,1,length=length(p)),p/sum(p*df),0,color=RGBA(0.5,0,0,0.2))
-
-for i ∈ 1:10
-    p = conv(P.(λ),p)
-end
-
-df = range(-5,1,length=length(p))[2] - range(-5,1,length=length(p))[1]
-band!(range(-5,1,length=length(p)),p/sum(p*df)/2,0,color=RGBA(0.5,0,0,0.2))
-
+xlims!(-5,2)
 figure
+
 save("figures/laplacian-spectrum.pdf",figure)
